af1fa743b24251153fbb606041f7c2d643912314
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-14 19:55:55 -0400

    feat: import movieController functions into movie router

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const router = express.Router();
 
+// Controllers
+const {
+  searchMovies,
+  getMovieDetails,
+} = require("../controllers/movieController");
+
 // Routes
 router.get("/search", searchMovies);
 router.get("/movies/:id", getMovieDetails);
 
 // Export router
 module.exports = router;
-

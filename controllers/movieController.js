466e1452051b72f63b8501b90a2f44dd262702f4
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-14 20:23:32 -0400

    feat: add title validation to searchMovies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -5,6 +5,10 @@ const axios = require("axios");
 const searchMovies = async (req, res) => {
   const { title } = req.query;
 
+  if (!title) {
+    return res.status(400).json({ error: "Title query parameter is required" });
+  }
+
   try {
     const response = await axios.get("http://www.omdbapi.com/", {
       params: {

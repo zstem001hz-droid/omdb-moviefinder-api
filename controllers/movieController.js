81e2713e19c6641145d444cf499d1bdd7c7a99d6
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-14 20:11:48 -0400

    feat: implement searchMovies controller function

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,13 +3,25 @@ const axios = require("axios");
 
 // Search Movies
 const searchMovies = async (req, res) => {
+  const { title } = req.query;
 
+  try {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        s: title,
+        apikey: process.env.OMDB_API_KEY,
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error searching movies:", error.message);
+    res.status(500).json({ error: "Failed to fetch movies." });
+  }
 };
 
 // Get Movie Details
-const getMovieDetails = async (req, res) => {
-
-};
+const getMovieDetails = async (req, res) => {};
 
 // Exports
 module.exports = { searchMovies, getMovieDetails };

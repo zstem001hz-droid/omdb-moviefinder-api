684a4fd77a16cca271e8cdf72b967e6722bd3331
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-14 20:01:32 -0400

    feat: create movieController with function scaffolding

---
+++ controllers/movieController.js
@@ -0,0 +1,15 @@
+// Dependencies
+const axios = require("axios");
+
+// Search Movies
+const searchMovies = async (req, res) => {
+
+};
+
+// Get Movie Details
+const getMovieDetails = async (req, res) => {
+
+};
+
+// Exports
+module.exports = { searchMovies, getMovieDetails };
